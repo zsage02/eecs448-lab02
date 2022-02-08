77f139176078e27f8b33fddb74e9332533c27ccc
Zach Sage <z103s534@ku.edu>
2022-02-08 07:23:09 -0600

    Updated search function (line 42 specifically) previously was temp == value now temp.getEntry() == value

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -39,7 +39,7 @@ bool LinkedList<T>::search(T value) const
 	bool isFound = false;
 	for(int i = 0; i < m_size; i++)
 	  {
-	    if(temp == value)
+	    if(temp.getEntry() == value)
 	      {
 		isFound = true;
 	      }
